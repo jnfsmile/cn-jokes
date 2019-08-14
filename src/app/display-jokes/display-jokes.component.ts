b33aaa69cebb74edb099e8d381ab80445aa48511
JNF <jnfsmile@gmail.com>
2019-08-14 09:08:52 +0300

    add colors

--- src/app/display-jokes/display-jokes.component.ts
+++ src/app/display-jokes/display-jokes.component.ts
@@ -12,6 +12,7 @@ export class DisplayJokesComponent implements OnInit {
 
   jokes;
   cat = "";
+  catColors = new Map();
 
   constructor(
     private route: ActivatedRoute,
@@ -29,6 +30,21 @@ export class DisplayJokesComponent implements OnInit {
   reloadJokes(num) {
     this.jokesService.getJokes(this.cat, num)
       .subscribe(jokes => this.jokes = jokes.value);
+  }
+
+  categoryColor(categories) {
+    if (categories.length === 0) return {};
+    if (!this.catColors.has(categories[0])) {
+      let color = this.brightColor();
+      this.catColors.set(categories[0], color)
+    }
+    return { "color": this.catColors.get(categories[0]) };
+  }
+
+  brightColor() {
+    return "hsl(" + 360 * Math.random() + ',' +
+      '100%,' +
+      (65 + 10 * Math.random()) + '%)';
   }
 
 }
--- src/app/display-jokes/jokes.service.ts
+++ src/app/display-jokes/jokes.service.ts
@@ -13,9 +13,7 @@ export class JokesService {
   jokes = { "type": "success", "value": [{ "id": 89, "joke": "For some, the left testicle is larger than the right one. For Chuck Norris, each testicle is larger than the other one.", "categories": ["explicit"] }, { "id": 495, "joke": "Chuck Norris doesn't needs try-catch, exceptions are too afraid to raise.", "categories": ["nerdy"] }, { "id": 436, "joke": "Chuck Norris describes human beings as &quot;a sociable holder for blood and guts&quot;.", "categories": [] }, { "id": 64, "joke": "There are no races, only countries of people Chuck Norris has beaten to different shades of black and blue.", "categories": [] }, { "id": 210, "joke": "Chuck Norris can judge a book by its cover.", "categories": [] }, { "id": 403, "joke": "Chuck Norris crossed the road. No one has ever dared question his motives.", "categories": [] }, { "id": 548, "joke": "Product Owners never argue with Chuck Norris after he demonstrates the DropKick feature.", "categories": ["nerdy"] }, { "id": 191, "joke": "The easiest way to determine Chuck Norris' age is to cut him in half and count the rings.", "categories": [] }, { "id": 187, "joke": "# Chuck Norris's show is called Walker: Texas Ranger, because Chuck Norris doesn't run.", "categories": [] }, { "id": 88, "joke": "Chuck Norris doesnt shave; he kicks himself in the face. The only thing that can cut Chuck Norris is Chuck Norris.", "categories": [] }] };
 
   getJokes(cat, qty = 10) {
-    let list = this.http.get(`${this.url}${qty}` + (cat ? `?limitTo=[${cat}]` : '')).pipe(
-      tap()
-    );
+    let list = this.http.get(`${this.url}${qty}` + (cat ? `?limitTo=[${cat}]` : ''));
 
     return list;
   }
